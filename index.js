93cab62272c9d34ac56193c368a49cd3cb57c659
Robert Akhmerov <r.akhmerov@corp.mail.ru>
2023-09-02 18:17:13 +0300

    [master] initial commit

--- index.js
+++ index.js
@@ -9,23 +9,20 @@ const push2 = {
 const canvas = createCanvas(push2.width, push2.height)
 const ctx = canvas.getContext('2d')
 
-function drawFrame(c, frameNum) {
-  c.fillStyle = "#000"
-  c.fillRect(0, 0, push2.width, push2.height)
-  c.font = '800 20px "SF Pro Display"';
-  c.fillStyle = "hsl(" + frameNum % 360 +",100%,50%)"
-  c.fillText("Привет, Женечка!", frameNum, Math.sin(frameNum/50)*push2.height*0.4+push2.height/2);
+function drawFrame(frameNum) {
+  ctx.fillStyle = "#000"
+  ctx.fillRect(0, 0, push2.width, push2.height)
+  ctx.font = '800 20px "SF Pro Display"';
+  ctx.fillStyle = "hsl(" + frameNum % 360 +",100%,50%)"
+  ctx.fillText("Привет, Женечка!", frameNum, Math.sin(frameNum/50)*push2.height*0.4+push2.height/2);
 }
 
 let frameNum = 0
 
 function nextFrame() {
-  drawFrame(ctx, frameNum)
+  drawFrame(frameNum)
   frameNum++
-  sendFrame(ctx, function(error) {
-    // we can ignore any error here, more or less
-    setTimeout(nextFrame,10) // Do not use nextTick here, as this will not allow usb callbacks to fire.
-  })
+  sendFrame(ctx, err => setTimeout(nextFrame,10))
 }
 
 initPush(err => {
