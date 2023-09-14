304d55c6de258b1a8735b9d3fc2df043876a4a64
Robert Akhmerov <r.akhmerov@corp.mail.ru>
2023-09-14 10:44:25 +0300

    [master] emoji fun

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const {createCanvas} = require('canvas')
-const path = require('path')
-const { initPush, sendFrame } = require('ableton-push-canvas-display')
-const ableton = require('ableton-push2');
-const robot = require('robotjs');
+const {createCanvas} = require('canvas');
+const {
+  initPush,
+  sendFrame,
+} = require('ableton-push-canvas-display');
+const abletonPush2Lib = require('ableton-push2');
+const robotJS = require('robotjs');
 
-const device = new ableton.Push2(port='user');
-//device.monitor();
+const push2 = new abletonPush2Lib.Push2(port = 'user');
+//push2.monitor();
 
 let fontMultiplier = 1;
 let speedMultiplier = 1;
 let waveSize = 1;
 
-
-device.setColor([1,8], 127);
-
-
-device.midi.on('message', (v) => {
+push2.setColor([1, 8], 127);
+push2.midi.on('message', (v) => {
   if (v._type === 'activesense') {
     return;
   }
@@ -25,85 +24,83 @@ device.midi.on('message', (v) => {
       const smiles = ['😊', '😄', '😆', '😂', '🤣'];
       const maxVelocity = 60;
       const smile = smiles[Math.floor(v.velocity * (smiles.length - 1) / maxVelocity)];
-      robot.typeString(smile)
+      robotJS.typeString(smile);
     }
   }
   // { channel: 0, value: 16320, _type: 'pitch' }
   if (v.channel === 0 && v._type === 'pitch') {
     const color = Math.round(v.value * 127 / 16320);
-    device.setColor([1,8], color);
-    console.log({color})
+    push2.setColor([1, 8], color);
+    console.log({color});
   }
   if (v.channel === 0 && v._type === 'cc') {
     if (v.controller === 71) {
       if (v.value === 1) {
-        fontMultiplier += 0.01
+        fontMultiplier += 0.01;
       } else if (v.value === 127) {
-        fontMultiplier -= 0.01
+        fontMultiplier -= 0.01;
       }
     }
     if (v.controller === 72) {
       if (v.value === 1) {
-        speedMultiplier += 0.01
+        speedMultiplier += 0.01;
       } else if (v.value === 127) {
-        speedMultiplier -= 0.01
+        speedMultiplier -= 0.01;
       }
     }
     if (v.controller === 73) {
       if (v.value === 1) {
-        waveSize += 0.01
+        waveSize += 0.01;
       } else if (v.value === 127) {
-        waveSize -= 0.01
+        waveSize -= 0.01;
       }
     }
   }
-  console.log(v)
+  console.log(v);
 });
 
+const width = 960;
+const height = 160;
 
-const push2 = {
-  width: 960,
-  height: 160,
-}
-const canvas = createCanvas(push2.width, push2.height)
-const ctx = canvas.getContext('2d')
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
 
 function drawFrame(frameNum) {
-  ctx.fillStyle = "#000"
-  ctx.fillRect(0, 0, push2.width, push2.height)
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, width, height);
   // draw 8 white column rects full push2.width:
-  ctx.fillStyle = "#111"
+  ctx.fillStyle = '#111';
   for (let i = 0; i < 8; i++) {
     if (i % 2 === 0) {
-      continue
+      continue;
     }
-    ctx.fillRect(i*push2.width/8, 0, push2.width/8, push2.height)
+    ctx.fillRect(i * width / 8, 0, width / 8, height);
   }
 
   // draw 4 white row rects full push2.height:
-  ctx.fillStyle = "rgba(255,255,255, 0.3)"
+  ctx.fillStyle = 'rgba(255,255,255, 0.3)';
   for (let i = 0; i < 4; i++) {
     if (i % 2 === 0) {
-      continue
+      continue;
     }
-    ctx.fillRect(0, i*push2.height/4, push2.width, push2.height/4)
+    ctx.fillRect(0, i * height / 4, width, height / 4);
   }
-  ctx.font = '800 ' + (20*fontMultiplier) + 'px "SF Pro Display"';
-  ctx.fillStyle = "hsl(" + frameNum % 360 +",100%,50%)"
-  ctx.fillText("Привет, Женечка!", (frameNum*speedMultiplier-100)%push2.width, Math.sin(frameNum/50)*push2.height*waveSize*0.4+push2.height/2);
+  ctx.font = '800 ' + (20 * fontMultiplier) + 'px "SF Pro Display"';
+  ctx.fillStyle = 'hsl(' + frameNum % 360 + ',100%,50%)';
+  ctx.fillText('Привет, Женечка!', (frameNum * speedMultiplier - 100) % width, Math.sin(frameNum / 50) * height * waveSize * 0.4 + height / 2);
 }
 
-let frameNum = 0
+let frameNum = 0;
 
 function nextFrame() {
-  drawFrame(frameNum)
-  frameNum++
-  sendFrame(ctx, err => setTimeout(nextFrame,10))
+  drawFrame(frameNum);
+  frameNum++;
+  sendFrame(ctx, err => setTimeout(nextFrame, 10));
 }
 
 initPush(err => {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  nextFrame()
-})
+  nextFrame();
+});
