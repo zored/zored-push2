7d60b2b58cc2737a36a9a3ea66971bb39cb1a0cd
Robert Akhmerov <r.akhmerov@corp.mail.ru>
2023-10-10 20:31:38 +0300

    [master]

--- src/flows/integrator.js
+++ src/flows/integrator.js
@@ -12,7 +12,8 @@ export class Integrator extends Flow {
       tool: 'peach',
       lib: 'orange',
     };
-    this.selected = {};
+    this.selectedRepo = null;
+    this.selectedButton = null;
     this.displayButtons = null;
     this.inputsInited = false;
   }
@@ -95,38 +96,25 @@ export class Integrator extends Flow {
         clickedColor = colors.blue;
 
       v.setColor(buttonColor);
-      v.listen(({up}) => {
+      v.listen(async ({up}) => {
         if (!up) {
           v.setColor(clickedColor);
           return;
         }
 
+
         // color and animation:
-        if (this.selected[name]) {
-          delete this.selected[name];
-          v.setAnimation('stopTransition');
+        this.selectedButton?.setAnimation('stopTransition');
+        if (this.selectedRepo === name) {
+          this.selectedRepo = null;
         } else {
-          this.selected[name] = true;
+          this.selectedRepo = name;
+          this.selectedButton = v;
           v.setAnimation('pulsingHalf');
         }
         v.setColor(buttonColor);
 
-        // change active display buttons:
-        // this.displayButtons.reset();
-        const selectedRepos = Object.keys(this.selected);
-        // this.device.displayCommand('intro', {text: selectedRepos.map(v => v.replace('-service', '')).join(' + ')}).then();
-        const fixes = selectedRepos.map(v => data.Fixes[v]);
-        if (!fixes.length) {
-          return;
-        }
-        // fixes.reduce((a, b) => (a || []).filter(c => (b || []).includes(c || [])))
-        //   ?.slice(0, 16)
-        //   ?.forEach((v, i) => this.displayButtons.setButton(v, async (button, v, index) => {
-        //     const r = await this.runFix(selectedRepos, button.text);
-        //     if (r && r.error) {
-        //       console.error(r);
-        //     }
-        //   }, i > 8 ? 1 : 0, i % 8));
+        await this.device.displayCommand('selectRepo', {name: this.selectedRepo});
       });
     });
   }
